a8a6ae1ba48c2ee85beb0452bff280acd7cb2709
MunzurulAzam <munzurul1998@gmail.com>
2023-01-18 22:06:19 +0600

    update9

--- app/src/main/java/azam/islamic_app/Todbir.java
+++ app/src/main/java/azam/islamic_app/Todbir.java
@@ -1,20 +1,45 @@
 package azam.islamic_app;
 
+import static androidx.appcompat.app.AppCompatDelegate.setDefaultNightMode;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
 public class Todbir extends AppCompatActivity {
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_todbir);
+
+        //color action bar start
+
+        setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+//        getSupportActionBar().hide();
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        getWindow().setStatusBarColor(this.getResources().getColor(R.color.white));
+
+        //color action bar end
+
+        //        start hook
+        imageView = findViewById(R.id.todbirBackBtn);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+//        hooks end
     }
 }
