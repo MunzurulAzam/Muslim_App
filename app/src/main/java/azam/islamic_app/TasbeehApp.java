2640d6ce62a940e486a1a4d4eda4c9fa7c6776e0
MunzurulAzam <munzurul1998@gmail.com>
2023-01-18 11:04:04 +0600

    update7

--- app/src/main/java/azam/islamic_app/TasbeehApp.java
+++ app/src/main/java/azam/islamic_app/TasbeehApp.java
@@ -25,10 +25,10 @@ public class TasbeehApp extends AppCompatActivity {
         setContentView(R.layout.activity_tasbeeh_app);
 
         TextView txtCount = findViewById(R.id.txtCounter);
-        TextView btnCount = findViewById(R.id.btnTasbeeh);
-        Button btn100 = findViewById(R.id.btn100);
+        Button btnCount = findViewById(R.id.btnTasbeeh);
+//        Button btn100 = findViewById(R.id.btn100);
         Button btnReset = findViewById(R.id.btnReset);
-        Button btnnoLimit = findViewById(R.id.btnNolimit);
+//        Button btnnoLimit = findViewById(R.id.btnNolimit);
 
         counter = 0;
         limit = 0;
@@ -38,11 +38,11 @@ public class TasbeehApp extends AppCompatActivity {
             public void onClick(View v) {
 //                counter++;
 
-                if(limit == 100 & counter <100){
-
-                    counter++;
-                }
-                else if (limit == 0){
+//                if(limit == 100 & counter <100){
+//
+//                    counter++;
+//                }
+                 if (limit == 0){
                     counter++;
                 }
 
@@ -57,19 +57,19 @@ public class TasbeehApp extends AppCompatActivity {
                 txtCount.setText(""+counter);
             }
         });
-        btn100.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                limit = 100;
-            }
-        });
+//        btn100.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                limit = 100;
+//            }
+//        });
 
-        btnnoLimit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                limit = 0;
-            }
-        });
+//        btnnoLimit.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                limit = 0;
+//            }
+//        });
 
     }
 }
