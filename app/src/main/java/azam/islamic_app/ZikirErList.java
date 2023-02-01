0cdc2a9251c12badbef4d6b4e5e7540655a22fb2
MunzurulAzam <munzurul1998@gmail.com>
2023-02-02 03:12:14 +0600

    update cont...

--- app/src/main/java/azam/islamic_app/ZikirErList.java
+++ app/src/main/java/azam/islamic_app/ZikirErList.java
@@ -2,12 +2,15 @@ package azam.islamic_app;
 
 public class ZikirErList {
     String zikir_name;
+    String zikir_Number;
+
 
     public ZikirErList() {
     }
 
-    public ZikirErList(String zikir_name) {
+    public ZikirErList(String zikir_name, String zikir_Number) {
         this.zikir_name = zikir_name;
+        this.zikir_Number = zikir_Number;
     }
 
     public String getZikir_name() {
@@ -16,5 +19,13 @@ public class ZikirErList {
 
     public void setZikir_name(String zikir_name) {
         this.zikir_name = zikir_name;
+    }
+
+    public String getZikir_Number() {
+        return zikir_Number;
+    }
+
+    public void setZikir_Number(String zikir_Number) {
+        this.zikir_Number = zikir_Number;
     }
 }
