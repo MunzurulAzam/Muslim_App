e1e75dbc930dbc2fd8fbc1c7f818517a9c3f9bb9
MunzurulAzam <munzurul1998@gmail.com>
2023-02-05 06:57:41 +0600

    update cont...

---
+++ app/src/main/java/azam/islamic_app/AmolErList.java
@@ -0,0 +1,14 @@
+package azam.islamic_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AmolErList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_amol_er_list);
+    }
+}
