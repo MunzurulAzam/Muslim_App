597ac00416d885d67f1f8f92b24bf7ab649254b7
MunzurulAzam <munzurul1998@gmail.com>
2023-02-06 06:19:32 +0600

    update cont...

--- app/src/main/java/azam/islamic_app/AmolList_Info.java
+++ app/src/main/java/azam/islamic_app/AmolList_Info.java
@@ -1,4 +1,31 @@
 package azam.islamic_app;
 
 public class AmolList_Info {
+    String amol_Number;
+    String amol_name;
+
+
+    public AmolList_Info() {
+    }
+
+    public AmolList_Info(String amol_Number, String amol_name) {
+        this.amol_Number = amol_Number;
+        this.amol_name = amol_name;
+    }
+
+    public String getAmol_Number() {
+        return amol_Number;
+    }
+
+    public void setAmol_Number(String amol_Number) {
+        this.amol_Number = amol_Number;
+    }
+
+    public String getAmol_name() {
+        return amol_name;
+    }
+
+    public void setAmol_name(String amol_name) {
+        this.amol_name = amol_name;
+    }
 }
