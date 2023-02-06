bbee611199dcfa89118fc23c0eedb1e1d28d94aa
MunzurulAzam <munzurul1998@gmail.com>
2023-02-06 06:21:25 +0600

    update cont...

--- app/src/main/java/azam/islamic_app/Amol.java
+++ app/src/main/java/azam/islamic_app/Amol.java
@@ -1,12 +1,19 @@
 package azam.islamic_app;
 
+import static androidx.appcompat.app.AppCompatDelegate.setDefaultNightMode;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
 
 public class Amol extends AppCompatActivity {
+
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +22,24 @@ public class Amol extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_amol);
+
+        setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+//        getSupportActionBar().hide();
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        getWindow().setStatusBarColor(this.getResources().getColor(R.color.white));
+
+
+        //        start hook
+        imageView = findViewById(R.id.quranerDuyaBackbtn);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+//        hooks end
+
+
+
     }
 }
